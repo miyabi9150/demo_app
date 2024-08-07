45ec7d454be43b25903756f71cf37a9cbf7012ea
miyabi9150 <miyabi9150@gmail.com>
2024-08-07 17:14:56 +0900

    Update page3.py

--- pages/page3.py
+++ pages/page3.py
@@ -9,8 +9,8 @@ df = pd.read_csv('./data/Temp.csv', index_col='月')
 st.line_chart(df)
 st.bar_chart(df['2021年'], x_label="月")
 
-# 13.matplotlob
-fig, ax = plt.subplots()
-ax.plot(df.index, df['2021年'])
-ax.set_title('matplotlib graph')
-st.pyplot(fig)
+# # 13.matplotlob
+# fig, ax = plt.subplots()
+# ax.plot(df.index, df['2021年'])
+# ax.set_title('matplotlib graph')
+# st.pyplot(fig)
