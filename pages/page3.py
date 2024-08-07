d703c5b2651c774d636ea6897309232fd2460ae3
miyabi9150 <miyabi9150@gmail.com>
2024-08-07 17:24:00 +0900

    Update page3.py

--- pages/page3.py
+++ pages/page3.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 
 # 12.データ分析関連のウィジェット
 df = pd.read_csv('./data/Temp.csv', index_col='月')
