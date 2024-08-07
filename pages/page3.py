6537ab90e8f05e063fe7f1ed6bf14f3caeb5f298
miyabi9150 <miyabi9150@gmail.com>
2024-08-07 17:36:57 +0900

    Update page3.py

--- pages/page3.py
+++ pages/page3.py
@@ -3,7 +3,7 @@ import pandas as pd
 # import matplotlib.pyplot as plt
 
 # 12.データ分析関連のウィジェット
-df = pd.read_csv('./data/Temp.csv', index_col='月')
+df = pd.read_csv('./data/平均気温.csv', index_col='月')
 # st.dataframe(df)
 # st.table(df)
 st.line_chart(df)
